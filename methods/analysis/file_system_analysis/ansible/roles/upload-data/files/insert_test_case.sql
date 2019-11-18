1139f5a06469b2fbcb5bbde2818f392503507ab6
František Hrdina <422195@mail.muni.cz>
2019-11-18 15:41:12 +0100

    Updated ansible role for initial testing case to work with notepad

--- methods/analysis/file_system_analysis/ansible/roles/upload-data/files/insert_test_case.sql
+++ methods/analysis/file_system_analysis/ansible/roles/upload-data/files/insert_test_case.sql
@@ -1,3 +1,3 @@
 INSERT INTO "case" (name, description) VALUES ('test', 'dataset that tests uploading');
 INSERT INTO "access" (user_id, case_id, role) VALUES (1, 1, 'admin');
-
+INSERT INTO "note" (text, case_id, user_id) VALUES ('Initial note', 1, 1);
