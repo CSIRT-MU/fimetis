0f0708865cdddc8afca8fe9ed4c54a701f82c8cb
Daniel Kouril <kouril@ics.muni.cz>
2023-11-14 14:58:36 +0100

    Enable disabling of authentication in the configuration

--- frontend/src/app/auth/auth.guard.ts
+++ frontend/src/app/auth/auth.guard.ts
@@ -2,15 +2,21 @@ import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { AuthenticationService } from './authentication.service';
+import { AppConfigService } from '../services/appconfig.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
     constructor(
         private router: Router,
-        private authenticationService: AuthenticationService
+        private authenticationService: AuthenticationService,
+        private appConfigService: AppConfigService
     ) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        if (this.appConfigService.getConfig().disable_authentication) {
+            return true;
+        }
+
         const currentUser = this.authenticationService.currentUserValue;
         if (currentUser) {
             // logged in so return true
--- frontend/src/app/services/appconfig.service.ts
+++ frontend/src/app/services/appconfig.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 export interface AppConfig {
+    disable_authentication: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
